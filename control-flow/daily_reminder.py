0e7b5f3b70cc67f6ddd84429ea70f57a95ab9d69
Olumide <adeoyeolumbah@gmail.com>
2025-06-01 14:13:03 +0100

    Made changes to the words that are case sensitive.

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -1,17 +1,17 @@
-task = input("Enter a task description: ")
-priority = input("Enter task's priority(high, medium, low: ")
-time_bound = input("Enter, if task is time-bound? yes or no: ")
-if time_bound == "yes":
- match priority:
+Task = input("Enter a task description: ")
+Priority = input("Enter task's priority(high, medium, low: ")
+Time_bound = input("Enter, if task is time-bound? yes or no: ")
+if Time_bound == "yes":
+ match Priority:
     case "high":
-     print(f"Reminder: {task} is a {priority} priority task that requires immediate attention today!")
+     print(f"Reminder: {Task} is a {Priority} priority task that requires immediate attention today!")
     case "medium":
-     print(f"Reminder: {task} is a {priority} priority task that requires immediate attention today!")
+     print(f"Reminder: {Task} is a {Priority} priority task that requires immediate attention today!")
     case "low":
-     print(f"Reminder: {task} is a {priority} priority task that requires attention today!")
-if time_bound == "no":
- match priority:
+     print(f"Reminder: {Task} is a {Priority} priority task that requires attention today!")
+if Time_bound == "no":
+ match Priority:
     case "low":
-     print (f"Note: {task} is a {priority} priority task. Consider completing it when you have free time.")
+     print (f"Note: {Task} is a {Priority} priority task. Consider completing it when you have free time.")
     case _:
-        print(f"Note: {task} is a {priority} priority task. Consider completing it when you have free time.")
+        print(f"Note: {Task} is a {Priority} priority task. Consider completing it when you have free time.")

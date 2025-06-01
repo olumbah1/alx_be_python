aadb3cd9762f6e3416a2990cf6226f936773c5a6
Olumide <adeoyeolumbah@gmail.com>
2025-06-01 11:14:05 +0100

    I repeated the match in every line of command which lead to error

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -5,13 +5,10 @@ type_of_operation = input("Choose the operation (+, -, *, /): ")
 match type_of_operation:
     case "+":
      print(f"{num1} + {num2} = {num1 + num2}")
-match type_of_operation:
     case "-":
      print(f"{num1} - {num2} = {num1 - num2}")
-match type_of_operation:
     case "*":
      print(f"{num1} * {num2} = {num1 * num2}")
-match type_of_operation:
     case "/":
      print(f"{num1} / {num2} = {num1 / num2}")
      if num2 != 0:

60155cbbd9fa69b90f3c16903c53abc33ea74cf1
Olumide <adeoyeolumbah@gmail.com>
2025-06-08 12:06:46 +0100

    Changed some line of code to return vslues.

--- fns_and_dsa/arithmetric_operations.py
+++ fns_and_dsa/arithmetric_operations.py
@@ -1,12 +1,12 @@
+  
 def perform_operation(num1: float, num2: float, operation: str):
     operations = {
         "add": lambda a, b: a + b,
         "subtract": lambda a, b: a - b,
         "multiply": lambda a, b: a * b,
-        "divide": lambda a, b: a / b if b != 0 else float('inf')
+        "divide": lambda a, b: a / b if b != 0 else "Error: Division by zero"
     }
     if operation in operations:
-     return operation[operation](num1, num2)
+        return operations[operation](num1, num2)
     else:
-     raise ValueError(f"Invalid operation: {operation}")
-   
+        return "Error: Invalid operation"

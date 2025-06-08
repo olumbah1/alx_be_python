65e390d1df123ef91ce641210779cc24d3792a4e
Olumide <adeoyeolumbah@gmail.com>
2025-06-08 13:43:38 +0100

    Adjust a line of code

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -1,9 +1,11 @@
+shopping_list = []
 def display_menu():
     print("\nShopping List Manager")
     print("1. Add Item")
     print("2. Remove Item")
     print("3. View List")
     print("4. Exit")
+print()
 
 def main():
     shopping_list = []

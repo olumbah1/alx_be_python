839a6a36be8d7dcb8612907ca866e17a4d558102
Olumide <adeoyeolumbah@gmail.com>
2025-06-08 12:25:02 +0100

    Adjusted the line of code

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,11 +1,14 @@
 def perform_operation(num1: float, num2: float, operation: str):
-    operations = {
-        "add": lambda a, b: a + b,
-        "subtract": lambda a, b: a - b,
-        "multiply": lambda a, b: a * b,
-        "divide": lambda a, b: a / b if b != 0 else "Error: Division by zero"
-    }
-    if operation in operations:
-        return operations[operation](num1, num2)
+    if operation == "add":
+        return num1 + num2
+    elif operation == "subtract":
+        return num1 - num2
+    elif operation == "multiply":
+        return num1 * num2
+    elif operation == "divide":
+        if num2 == 0:
+            return "Error: Division by zero"
+        return num1 / num2
     else:
         return "Error: Invalid operation"
+

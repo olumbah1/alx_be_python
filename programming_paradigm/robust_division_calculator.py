700c9bcebe39e5cf880f49943113510b8ef6ab17
Olumide <adeoyeolumbah@gmail.com>
2025-06-15 00:31:36 +0100

    Corrected some line of code

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -1,7 +1,7 @@
 def safe_divide (numerator, denominator):
     try:
-        num = float(numerator)
-        den = float(denominator)
+        numerator = float(numerator)
+        denominator = float(denominator)
         result = numerator / denominator
         return f"The result of the division is {result}"
     except ZeroDivisionError:

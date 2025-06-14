9b293a7e4714ae0bbafc4d279fccbd553a12141a
Olumide <adeoyeolumbah@gmail.com>
2025-06-14 22:34:43 +0100

    Corrected the line of code

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -12,6 +12,6 @@ class BankAccount:
             return True
         return False
     def display_balance(self):
-        print(f"Current balance:{self.account_balance:}")
+        print(f"Current Balance:{self.account_balance:}")
 
                  

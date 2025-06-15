273374fddbcca3a51cd0f7c739abb3a1d5ad2607
Olumide <adeoyeolumbah@gmail.com>
2025-06-15 10:37:35 +0100

    Corrected a use case

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -11,7 +11,7 @@ class TestSimpleCalculator(unittest.TestCase):
        self.assertEqual(self.calc.add(-1, 1), 0)
        self.assertEqual(self.calc.add(-5, 3), -2)
 
-    def test_subtract(self):
+    def test_subtraction(self):
         self.assertEqual(self.calc.subtract(5, 2), 3)
         self.assertEqual(self.calc.subtract(10, 3), 7)
         self.assertEqual(self.calc.subtract(-1, -3), -4)

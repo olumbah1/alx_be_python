52f3a28f9ea22213f45e4c0a6d75f0e8159e0243
Olumide <adeoyeolumbah@gmail.com>
2025-06-22 11:31:40 +0100

    Lines of code for shape(rectangle and circle) calculation

--- oop/polymorphism_demo.py
+++ oop/polymorphism_demo.py
@@ -14,7 +14,7 @@ class Rectangle(Shape):
         self.width = width
           
     def area(self):
-        return self.width * self.length
+        return self.length * self.width
         
     def __str__(self):
         return f'The area of the Rectangle is: {self.area()}'
